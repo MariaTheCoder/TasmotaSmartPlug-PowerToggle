5aa6bd5a2ecb365d0577adb0ef663d1c9455c579
Maria T. Andersen <mteilmann93@gmail.com>
2023-07-18 10:15:52 +0200

    In the frontend, use fetch to get the raw data at "/api" and try to display the data on the console. Notice that our fetch request was blocked due to cors request related issues. We can fix this in the backend by allowing all cors requests.

--- public/frontend.js
+++ public/frontend.js
@@ -1,3 +1,15 @@
 const flexContainer = document.getElementById("flex-container");
 
+function fetchPowerState() {
+  fetch("http://localhost:5600/api", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
 console.log(flexContainer);
+
+fetchPowerState();
